b36495d98a1fb9c283ab506617711c8a0c09aadf
jaylinwang <jaylin0019@gmail.com>
2023-11-07 20:31:29 +0800

    feat: change to vercel edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://jaylin.wang',
   integrations: [mdx(), sitemap()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    edgeMiddleware: true,
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
+  })
 });
