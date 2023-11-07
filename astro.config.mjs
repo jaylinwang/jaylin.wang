43a695297c01c9db93fb7bb3efa1f4b4bbcd5f1e
jaylinwang <jaylin0019@gmail.com>
2023-11-07 21:06:05 +0800

    feat: format code with prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jaylin.wang',
   integrations: [mdx(), sitemap()],
-  output: "server",
+  output: 'server',
   adapter: vercel({
     edgeMiddleware: true,
     webAnalytics: {
@@ -17,5 +17,5 @@ export default defineConfig({
     speedInsights: {
       enabled: true,
     },
-  })
+  }),
 });
