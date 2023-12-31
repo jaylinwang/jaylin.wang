2bebfbbd3b5c568af6102c8cca122b478e76b78e
jaylinwang <jaylin0019@gmail.com>
2023-12-31 23:49:24 +0800

    feat:更新文章

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-  type: 'content', 
+const post = defineCollection({
+  type: 'content',
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
@@ -10,4 +10,4 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+export const collections = { post };
